261b0cf1c357cbda05cbc5f78ef0dd17df9e9273
Tim Chin <timchin@Tims-MacBook-Pro.local>
2017-01-14 14:40:29 -0800

    Merges Non-FB Auth

--- server/server.js
+++ server/server.js
@@ -495,7 +495,7 @@ app.delete('/api/users/:userID', function(req, res) {
  *             *
  * * * * * * * */
 
-// GET all friends pins
+// GET all PRIVATE Pins
 app.get('/api/users/:userID/pins/private', function(req, res) {
   let userID = req.params.userID;
 
@@ -515,10 +515,7 @@ app.get('/api/users/:userID/pins/private', function(req, res) {
     });
 });
 
-// UNION MATCH (a)\
-//       WHERE (a)<-[:PINNED]-(n)-[:FRIENDED]->(n: User { userID:{userIDParam} })\
-//       RETURN a
-// GETS all public pins
+// Gets all PUBLIC Pins
 app.get('/api/users/:userID/pins/public', function(req, res) {
   let userID = req.params.userID;
 
